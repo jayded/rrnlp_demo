5bed7c0ed585adc410031dc9535ff544103ad204
Jay DeYoung <deyoung.j@husky.neu.edu>
2024-10-23 23:39:33 +0000

    Database fixing

--- rrnlp/app/database_utils.py
+++ rrnlp/app/database_utils.py
@@ -395,8 +395,8 @@ def get_auto_evidence_map_from_topic_uid(topic_uid):
     query = '''
         SELECT
             pubmed_extractions_ico_re.pmid,
-            pubmed_data.title AS title,
-            pubmed_data.abstract as abstract,
+            pubmed_data.titles AS title,
+            pubmed_data.abstracts as abstract,
             pubmed_extractions_ico_re.intervention,
             pubmed_extractions_ico_re.comparator,
             pubmed_extractions_ico_re.outcome,
@@ -407,7 +407,7 @@ def get_auto_evidence_map_from_topic_uid(topic_uid):
         FROM pubmed_extractions_ico_re
         INNER JOIN search_screening_results ON pubmed_extractions_ico_re.pmid = search_screening_results.pmid
         INNER JOIN pubmed_extractions ON pubmed_extractions.pmid = search_screening_results.pmid
-        OUTER JOIN pubmed_data on pubmed_data.pmid = search_screening_results.pmid
+        LEFT JOIN pubmed_data on pubmed_data.pmid = search_screening_results.pmid
         WHERE search_screening_results.topic_uid = :topic_uid
         AND search_screening_results.human_decision = "Include"
         ORDER BY search_screening_results.robot_ranking DESC
