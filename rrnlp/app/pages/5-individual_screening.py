6267936304e16bd79765c1c33f7e324fb4ff1ece
Jay DeYoung <deyoung.j@husky.neu.edu>
2024-10-26 16:27:51 +0000

    Switch up page execution to fix lagging DB inserts

--- rrnlp/app/pages/5-individual_screening.py
+++ rrnlp/app/pages/5-individual_screening.py
@@ -17,51 +17,105 @@ if st.session_state.topic_information['final'] != 1:
 
 if 'current_screening' not in st.session_state.topic_information:
     st.session_state.topic_information['current_screening'] = {}
+    st.session_state.topic_information['current_screening']['screened'] = set()
 
-if 'pmids' not in st.session_state.topic_information['current_screening'] or len(st.session_state.topic_information['current_screening']['pmids']) == 0:
-    if 'df' not in st.session_state.topic_information or 'article_data_df' not in st.session_state.topic_information:
-        _, _, article_data_df, df = database_utils.get_persisted_pubmed_search_and_screening_results(st.session_state.topic_information['topic_uid'])
-        st.session_state.topic_information['article_data_df'] = article_data_df
-        st.session_state.topic_information['df'] = df
-    df = st.session_state.topic_information['df']
-    df_ = df[df['human_decision'] == 'Unscreened']
-    to_screen_pmids = df_['pmid'].tolist()
-    # remove duplicates but retain order
-    to_screen_pmids = list({x:x for x in to_screen_pmids}.keys())
-    st.session_state.topic_information['current_screening']['pmids'] = to_screen_pmids
+print('individual article screening')
+for x in ['topic_name', 'topic_uid', 'search_text', 'search_query', 'generated_query', 'used_cochrane_filter', 'used_robot_reviewer_rct_filter', 'final']:
+    print(x, st.session_state.topic_information.get(x, 'none'))
 
-article_data_df = st.session_state.topic_information['article_data_df']
+for x in ['current_pmid']:
+    print(x, st.session_state.topic_information['current_screening'].get(x, 'none'))
 
-if 'current_pmid' not in st.session_state.topic_information['current_screening']:
-    if len(st.session_state.topic_information['current_screening']['pmids']) > 0:
-        st.session_state.topic_information['current_screening']['current_pmid'] = st.session_state.topic_information['current_screening']['pmids'].pop()
-    else:
-        st.markdown('No more pmids to screen!')
-        if st.button("Return to bulk screening", use_container_width=True):
-            st.switch_page('pages/4-search_results_and_screening.py', use_container_width=True)
-
-        if st.button("View Evidence Map", use_container_width=True):
-            st.switch_page('pages/6-evidence_map.py')
-        st.stop()
-
-this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
-this_article_data_len = len(this_article)
-
-while this_article_data_len == 0:
-    print(f"Skipping {st.session_state.topic_information['current_screening']['current_pmid']}")
-
-    st.session_state.topic_information['current_screening']['current_pmid'] = st.session_state.topic_information['current_screening']['pmids'].pop()
-    this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
-    this_article_data_len = len(this_article)
-
-st.write(f"""<a href="https://pubmed.ncbi.nlm.nih.gov/{st.session_state.topic_information['current_screening']['current_pmid']}">PMID{st.session_state.topic_information['current_screening']['current_pmid']}</a>""", unsafe_allow_html=True)
-
-this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
-assert len(this_article) == 1
 
 column1, column2 = st.columns([.75, .25])
 
+def insert(pmid, decision):
+    database_utils.insert_topic_human_screening_pubmed_results(st.session_state.topic_information['topic_uid'], {pmid: decision})
+    df = st.session_state.topic_information['df']
+    # this line is to keep state with the bigger screening dataframe. It's our (local) source of truth.
+    df[df['pmid'] == pmid]['human_decision'] = decision
+
+# Note to any future developers:
+# - the order here is important due to streamlit's execution order.
+# - First the page executes, and there have been no decisions
+# - then when someone makes a selection, the page re-executes
+# - by having all the logic of fetching the next topic and information happen _after_ the button actions execute, we can get the next article ready.
+with column2:
+    include = st.button(
+        'Include',
+        use_container_width=True,
+        icon=":material/thumb_up:",
+        on_click=lambda: insert(st.session_state.topic_information['current_screening']['current_pmid'], 'Include'),
+    )
+    exclude = st.button(
+        'Exclude',
+        use_container_width=True,
+        icon=":material/thumb_down:",
+        on_click=lambda: insert(st.session_state.topic_information['current_screening']['current_pmid'], 'Exclude'),
+    )
+    skip = st.button(
+        'Skip',
+        use_container_width=True,
+        icon=":material/question_mark:",
+    )
+
+    if include or exclude or skip:
+        st.session_state.topic_information['current_screening']['screened'].add(st.session_state.topic_information['current_screening']['current_pmid'])
+        del st.session_state.topic_information['current_screening']['current_pmid']
+
+    if st.button("Return to bulk screening", use_container_width=True):
+        st.switch_page('pages/4-search_results_and_screening.py')
+
+    if st.button("View Evidence Map", use_container_width=True):
+        st.switch_page('pages/6-evidence_map.py')
+
 with column1:
+    if 'pmids' not in st.session_state.topic_information['current_screening'] or len(st.session_state.topic_information['current_screening']['pmids']) == 0:
+        if 'df' not in st.session_state.topic_information or 'article_data_df' not in st.session_state.topic_information:
+            _, _, article_data_df, df = database_utils.get_persisted_pubmed_search_and_screening_results(st.session_state.topic_information['topic_uid'])
+            st.session_state.topic_information['article_data_df'] = article_data_df
+            st.session_state.topic_information['df'] = df
+        df = st.session_state.topic_information['df']
+        st.session_state.topic_information['current_screening']['screened'].update(df[df['human_decision'] != 'Unscreened'])
+        df_ = df[df['human_decision'] == 'Unscreened']
+
+        to_screen_pmids = df_['pmid'].tolist()
+        # remove duplicates but retain order
+        to_screen_pmids = list({x:x for x in to_screen_pmids}.keys())
+        st.session_state.topic_information['current_screening']['pmids'] = to_screen_pmids
+
+    article_data_df = st.session_state.topic_information['article_data_df']
+
+    if 'current_pmid' in st.session_state.topic_information['current_screening'] and st.session_state.topic_information['current_screening']['current_pmid'] in st.session_state.topic_information['current_screening']['screened']:
+        del st.session_state.topic_information['current_screening']['current_pmid']
+
+    if 'current_pmid' not in st.session_state.topic_information['current_screening']:
+        if len(st.session_state.topic_information['current_screening']['pmids']) > 0:
+            st.session_state.topic_information['current_screening']['current_pmid'] = st.session_state.topic_information['current_screening']['pmids'].pop()
+        else:
+            st.markdown('No more pmids to screen!')
+            if st.button("Return to bulk screening", use_container_width=True):
+                st.switch_page('pages/4-search_results_and_screening.py', use_container_width=True)
+
+            if st.button("View Evidence Map", use_container_width=True):
+                st.switch_page('pages/6-evidence_map.py')
+            st.stop()
+
+    this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
+    this_article_data_len = len(this_article)
+
+    while this_article_data_len == 0:
+        print(f"Skipping {st.session_state.topic_information['current_screening']['current_pmid']}")
+
+        st.session_state.topic_information['current_screening']['current_pmid'] = st.session_state.topic_information['current_screening']['pmids'].pop()
+        this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
+        this_article_data_len = len(this_article)
+
+    st.write(f"""<a href="https://pubmed.ncbi.nlm.nih.gov/{st.session_state.topic_information['current_screening']['current_pmid']}">PMID{st.session_state.topic_information['current_screening']['current_pmid']}</a>""", unsafe_allow_html=True)
+
+    this_article = article_data_df[article_data_df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]
+    assert len(this_article) == 1
+
     # TODO should this look like a pubmed page?
     st.markdown(this_article['titles'].tolist()[0])
     st.markdown(this_article['abstracts'].tolist()[0])
@@ -95,31 +149,3 @@ with column1:
         )
 
 
-with column2:
-    # TODO pretty the buttons
-    include = st.button('Include', use_container_width=True)
-    exclude = st.button('Exclude', use_container_width=True)
-    skip = st.button('Skip', use_container_width=True)
-
-    if include:
-        decision = 'Include'
-        assert not exclude
-    elif exclude:
-        decision = 'Exclude'
-
-    if include or exclude:
-        database_utils.insert_topic_human_screening_pubmed_results(st.session_state.topic_information['topic_uid'], {st.session_state.topic_information['current_screening']['current_pmid']: decision})
-        df = st.session_state.topic_information.get('df', None)
-        # this line is to keep state with the bigger screening dataframe. It's our (local) source of truth.
-        if df is not None:
-            df[df['pmid'] == st.session_state.topic_information['current_screening']['current_pmid']]['human_decision'] = decision
-        if 'screening_results' in st.session_state.topic_information:
-            del st.session_state.topic_information['screening_results']
-
-        del st.session_state.topic_information['current_screening']['current_pmid']
-
-    if st.button("Return to bulk screening", use_container_width=True):
-        st.switch_page('pages/4-search_results_and_screening.py')
-
-    if st.button("View Evidence Map", use_container_width=True):
-        st.switch_page('pages/6-evidence_map.py')
