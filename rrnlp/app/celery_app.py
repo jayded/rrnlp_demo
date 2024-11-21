359b8dcc9f05f38f02049990af220088ea3af951
Jay DeYoung <deyoung.j@husky.neu.edu>
2024-11-21 03:41:17 +0000

    Switch to clean task definitions and loading

--- rrnlp/app/celery_app.py
+++ rrnlp/app/celery_app.py
@@ -7,12 +7,12 @@ import time
 from os.path import abspath, dirname
 from typing import List
 
+import celery
 import yaml
 
-from celery import Celery
+from celery import Celery, shared_task, Task
 from yaml.loader import SafeLoader
 
-#from rrnlp.app.utils import get_searcher
 import rrnlp.models.SearchBot as SearchBot
 import rrnlp.models.MultiSummaryGenerationBot as MultiSummaryGenerationBot
 
@@ -29,18 +29,23 @@ result_backend = f'db+sqlite:////{base_dir}/backend.sqlite3'
 app = Celery('rrnlp',
              broker=sqlite_path,
              backend=result_backend,
-             #backend='rpc://',
-             #include=['tasks'])
 )
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        #logging.FileHandler('celery.log'),
         logging.StreamHandler()
     ]
 )
+
+
+@celery.signals.after_setup_logger.connect
+def on_after_setup_logger(**kwargs):
+    logger = logging.getLogger('celery')
+    logger.propagate = True
+    logger = logging.getLogger('celery.app.trace')
+    logger.propagate = True
 
 # Optional configuration, see the application user guide.
 app.conf.update(
@@ -50,57 +55,47 @@ app.conf.update(
 logging.info(f'celery paths: broker: {sqlite_path}, backend: {result_backend}')
 logging.info(f'Consider if sqlite is still appropriate for these paths')
 
-_model_loaded = False
 
-@app.on_after_configure.connect
-def setup_text_generation(sender, **kwargs):
-    global searcher, mds_summarizer, _model_loaded
-    if _model_loaded:
-        logging.info(f'skipping loading, model already loaded')
-    else:
-        _model_loaded = True
+class SearchTask(Task):
+    def __init__(self):
+        print('loading searcher')
         get_searcher_start = time.time()
-        #searcher = SearchBot.PubmedQueryGeneratorBot()
-        searcher = SearchBot.get_topic_to_pubmed_converter(
-            #weights=st.config['search_bot']['model_path'],
-            #tokenizer=st.config['search_bot']['tokenizer'],
-            #device='cpu',
-        )
+        self.searcher = SearchBot.get_topic_to_pubmed_converter()
         get_searcher_end = time.time()
         logging.info(f'loading the searcher took {get_searcher_end - get_searcher_start} seconds')
+
+    def compute(self, search_text):
+        generate_start = time.time()
+        logging.info(f'model data: {id(self.searcher)}')
+        logging.info(f'search_text: {search_text}')
+        query = self.searcher.generate_review_topic(search_text)
+        generate_end = time.time()
+        logging.info(f'generating the query took {generate_end - generate_start} seconds: {query}')
+        return query
+
+@app.task(base=SearchTask, name='generate_search', acks_late=False, bind=True)
+def generate_search(self: Task, search_text: str):
+    return self.compute(search_text)
+
+class MDSSummarizer(Task):
+    def __init__(self):
         get_summarizer_start = time.time()
         logging.info(f'loading key from {config["openai_api_key"]}')
-        mds_summarizer = MultiSummaryGenerationBot.load_openai_summary_bot(api_key_file = config['openai_api_key'])
-        #mds_summarizer = MultiSummaryGenerationBot.load_mds_summary_bot()
+        self.mds_summarizer = MultiSummaryGenerationBot.load_openai_summary_bot(api_key_file = config['openai_api_key'])
         get_summarizer_end = time.time()
         logging.info(f'loading the mds summarizer took {get_summarizer_end - get_summarizer_start} seconds')
-        _model_loaded = True
 
-@app.task
-def generate_search(search_text):
-    generate_start = time.time()
-    #with app.search_semaphore:
-    query = searcher.generate_review_topic(search_text)
-    generate_end = time.time()
-    logging.info(f'generating the query took {generate_end - generate_start} seconds: {query}')
-    return query
- 
-@app.task
-def mds_summarize(search_text, input_texts: List[str]):
-    generate_start = time.time()
-    #with app.search_semaphore:
-    summary, response = mds_summarizer.predict_for_docs(search_text, input_texts)
-    generate_end = time.time()
-    logging.info(f'generating the summary took {generate_end - generate_start} seconds')
-    return summary
-    
-## Other RRNlp generations
-#@app.task
-#def process_pmid(pmid):
-#    # TODO: all the things
-#    pass
+    def compute(self, search_text: str, input_texts: List[str]):
+        generate_start = time.time()
+        summary, response = self.mds_summarizer.predict_for_docs(search_text, input_texts)
+        generate_end = time.time()
+        logging.info(f'generating the summary took {generate_end - generate_start} seconds')
+        return summary
+
+@app.task(name='mds_summarize', acks_late=False)
+def mds_summarize(self, search_text, input_texts: List[str]):
+    return self.compute(search_text, input_texts)
 
 if __name__ == '__main__':
-    #app.search_semaphore = threading.Semaphore(1)
     app.start(argv=sys.argv[1:])
 
